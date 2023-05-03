402be490dfd93da9e4e35cb211c8e7a84ddc91ac
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-03 15:40:09 +0900

    sum1to10() 작성

--- src/main/java/com/ohgiraffers/level01/basic/Calculator.java
+++ src/main/java/com/ohgiraffers/level01/basic/Calculator.java
@@ -2,7 +2,16 @@ package com.ohgiraffers.level01.basic;
 
 public class Calculator {
     public void checkMethod(){
-        System.out.println("메소드 호출 확인")
+        System.out.println("메소드 호출 확인");
+    }
+
+    public int sum1to10(){
+        int sum = 0;
+
+        for(int i = 1; i <= 10; i++){
+            sum += i;
+        }
+        return sum;
     }
 
 }
