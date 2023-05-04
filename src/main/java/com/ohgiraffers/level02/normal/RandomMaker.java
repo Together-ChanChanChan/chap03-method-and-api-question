807577fc06f54073a641007ae85b7f333684df7e
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-04 13:42:42 +0900

    randomUpperAlpabet 메소드 생성

--- src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
+++ src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
@@ -1,4 +1,19 @@
 package com.ohgiraffers.level02.normal;
 
 public class RandomMaker {
+    public int randomNumber(int min, int max){
+        return (int)(Math.random() * (max-min) + 1) + min;
+    }
+
+    public String randomUpperAlpabet(int length){
+        String result = "";
+
+        length = 10;
+
+        for(int i = 0; i < length; i++) {
+            int c = (int)(Math.random() * 26) + 65;
+            result += (char)c;
+        }
+        return result;
+    }
 }
