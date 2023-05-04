700f562a3267967f43a07d2c86dc2bad82977efa
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-04 17:40:22 +0900

    Application 클래스 작성

--- src/main/java/com/ohgiraffers/level02/normal/Application.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application.java
@@ -2,6 +2,11 @@ package com.ohgiraffers.level02.normal;
 
 public class Application {
     public static void main(String[] args) {
+        RandomMaker rm = new RandomMaker();
 
+        System.out.println(rm.randomNumber(4,100));
+        System.out.println(rm.randomUpperAlpabet(5));
+        System.out.println(rm.rockPaperScissors());
+        System.out.println(rm.tossCoin());
     }
 }
--- src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
+++ src/main/java/com/ohgiraffers/level02/normal/RandomMaker.java
@@ -21,4 +21,9 @@ public class RandomMaker {
         String[] arr = {"가위", "바위", "보"};
         return arr[(int) (Math.random() * 3)];
     }
+
+    public String tossCoin() {
+        String[] arr = {"앞면", "뒷면"};
+        return arr[(int)(Math.random() * 2)];
+    }
 }
