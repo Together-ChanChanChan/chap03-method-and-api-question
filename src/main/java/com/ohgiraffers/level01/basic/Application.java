7bb9a77807be83c9933667513e154be11a8725ee
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-04 13:07:50 +0900

    .

--- src/main/java/com/ohgiraffers/level01/basic/Application.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application.java
@@ -1,7 +1,14 @@
 package com.ohgiraffers.level01.basic;
 
+
 public class Application {
     public static void main(String args[]){
+        Calculator calc = new Calculator();     //클래스 가져와 객체화한다.
 
+        calc.checkMethod();
+        calc.sum1to10();
+        calc.checkMaxNumber(10,20);
+        calc.sumTwoNumber(10,20);
+        calc.minusTwoNumber(10,5);
     }
 }
--- src/main/java/com/ohgiraffers/level01/basic/Calculator.java
+++ src/main/java/com/ohgiraffers/level01/basic/Calculator.java
@@ -7,11 +7,22 @@ public class Calculator {
 
     public int sum1to10(){
         int sum = 0;
-
         for(int i = 1; i <= 10; i++){
             sum += i;
         }
+
         return sum;
     }
 
+    public void checkMaxNumber(int a, int b){
+        System.out.println("두 수 중 큰 수는 " + Math.max(a, b) + "이다.");
+    }
+
+    public int sumTwoNumber(int a, int b){
+        return a + b;
+    }
+
+    public int minusTwoNumber(int a, int b){
+        return a-b;
+    }
 }
