64112d7757f24bf04593b145cc4172c5ac16de5a
Dahee <leedh9129@gmail.com>
2023-05-19 09:10:15 +0900

    method

---
+++ src/main/java/com/greedy/level01/basic/Application.java
@@ -0,0 +1,11 @@
+package com.greedy.level01.basic;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("메소드 호출 확인");
+
+        System.out.println("1부터 10까지의 합 : " );
+    }
+}
---
+++ src/main/java/com/greedy/level01/basic/Calculator.java
@@ -0,0 +1,10 @@
+package com.greedy.level01.basic;
+
+public class Calculator {
+
+    private static void checkMethod() {
+
+    }
+
+
+}
